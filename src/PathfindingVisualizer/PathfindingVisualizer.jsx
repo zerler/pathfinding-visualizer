c17f862a480aea5733f1317c4db2a51fe9abd6bb
Ben Zerler <zerler@wisc.edu>
2020-01-07 14:02:26 -0600

    initial commit

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './PathfindingVisualizer.css';
+
+export default class PathfindingVisualizer extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Foo</h1>
+      </div>
+    );
+  }
+}
